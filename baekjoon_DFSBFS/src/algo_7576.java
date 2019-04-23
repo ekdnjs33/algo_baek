0f03209bceffa30fe33c4632699dd052faf92906
ekdnjs33 <gwi02029@gmail.com>
2019-04-23 10:04:34 +0900

    modified code

--- baekjoon_DFSBFS/src/algo_7576.java
+++ baekjoon_DFSBFS/src/algo_7576.java
@@ -1,6 +1,6 @@
 
-/* 백준 7576 토마토
-* 다 못 품, 다시 풀기
+/* 백준 7576 토마토 1
+* bfs 이용
 * */
 
 import java.util.LinkedList;
@@ -8,48 +8,86 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class algo_7576 {
+    static int[] dx = {0, 1, 0, -1}; //우, 하, 좌, 상
+    static int[] dy = {1, 0, -1, 0};
     public static int[][] array;
-    public static int count;
+    public static int[][] visit; //방문 여부
+    public static int count; //모두 익는 최소 일수
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int M = scan.nextInt(); //가로
         int N = scan.nextInt(); //세로
+        boolean zero = false;
 
         array = new int[N][M];
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < M; j++)
+        visit = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 array[i][j] = scan.nextInt();
+            }
+        }
 
+        count = bfs(N, M);
+        /*for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                System.out.print(array[i][j]+ " ");
+            }
+            System.out.println();
+        }*/
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(array[i][j] == 0){
+                    zero = true;
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+        if(zero == false) {
+            System.out.println(count);
+        }
     }
 
-    static int bfs(int N, int K, int[] Min){
-        int nextN = N;
-        int[] status = new int[3];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(nextN);
-        Min[nextN] = 0; //몇 번 만에 방문이 가능한지 표시
+    static int bfs(int N, int M) {
+        boolean is = false;
+        int x = 0, y = 0;
+        Queue<Integer> qx = new LinkedList<>();
+        Queue<Integer> qy = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (array[i][j] == 1) {
+                    qx.add(i);
+                    qy.add(j);
+                } else if (array[i][j] == 0) {
+                    is = true;
+                }
+            }
+        }
+        if (is == false) {
+            return 0;
+        }
 
-        while(!q.isEmpty()){ //큐가 비면 종료
-            nextN = q.poll(); //큐에서 꺼내온 값
+        while (!qx.isEmpty() && !qy.isEmpty()) { //큐가 비면 종료
+            x = qx.poll();
+            y = qy.poll();
+            visit[x][y] = 1;
 
-            //다음에 이동할 좌표들
-            status[0] = nextN - 1;
-            status[1] = nextN + 1;
-            status[2] = nextN * 2;
+            for (int i = 0; i < 4; i++) {
+                int X = x + dx[i];
+                int Y = y + dy[i];
 
-            for(int i=0; i<3; i++){
                 //배열을 벗어나지 않았는지 확인
-                if(status[i] >= 0 && status[i] <= 100000){
+                if (X >= 0 && Y >= 0 && X < N && Y < M) {
                     //이전에 방문했는지 확인
-                    if(Min[status[i]] == -1){
-                        //처음 간 곳이라면 큐에 넣고, 전 위치값 +1해서 저장
-                        q.add(status[i]);
-                        Min[status[i]] = Min[nextN] + 1;
+                    if (visit[X][Y] == 0 && array[X][Y] == 0) {
+                        qx.add(X);
+                        qy.add(Y);
+                        array[X][Y] = array[x][y] + 1;
                     }
                 }
             }
         }
-        return Min[K];
+        return array[x][y] - 1;
     }
 }
